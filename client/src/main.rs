f12e3d9fe35ed1b36147e1df15f0b7c068ad776f
Ryusei Nohara <nhrryi@gmail.com>
2023-02-26 22:37:42 +0900

    wip

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,12 +1,20 @@
 use std::io::prelude::*;
 use std::os::unix::net::UnixStream;
+use std::time::Duration;
 
 fn main() -> std::io::Result<()> {
     println!("hello client");
 
     let socket_path = "/socket_file";
 
-    let mut stream = UnixStream::connect(socket_path).expect("Failed connection to server");
+    let mut stream = UnixStream::connect(socket_path).expect("Connection is failed to server");
+    stream
+        .set_nonblocking(true)
+        .expect("Couldn't set nonblocking");
+    stream
+        .set_read_timeout(Some(Duration::new(2, 0)))
+        .expect("Couldn't set read timeout");
+
     stream.write_all(b"hello world")?;
 
     let mut response = String::new();
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,17 @@
 use std::fs;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::os::unix::net::{UnixListener, UnixStream};
 use std::path::Path;
 use std::thread;
 
-fn handle_client(mut stream: UnixStream) {
+fn handle_client(mut stream: UnixStream) -> std::io::Result<()> {
     let mut message = String::new();
-    stream.read_to_string(&mut message);
-    println!("{:?}", message);
+    stream.read_to_string(&mut message)?;
+
+    println!("We received this message: {}\nReplying...", message);
+    stream.write(b"hello client")?;
+
+    Ok(())
 }
 
 fn main() -> std::io::Result<()> {
@@ -25,7 +29,7 @@ fn main() -> std::io::Result<()> {
     loop {
         let (mut unix_stream, socket_address) = listener.accept()?;
         println!("connection from {:?}", socket_address);
-        handle_client(unix_stream);
+        thread::spawn(|| handle_client(unix_stream));
     }
 
     Ok(())
