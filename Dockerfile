b0fbaf64fc5953a480268759fe7b385f52ab71f5
Ryusei Nohara <nhrryi@gmail.com>
2023-03-07 22:38:19 +0900

    refactor

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,24 @@
 FROM ryusei1068/rust_ubuntu:latest as build
 
-# build for sever program
+# build for sever side 
 RUN USER=root cargo new --bin server
 WORKDIR /server
 COPY ./serverCargo* ./
 COPY ./server/src ./src
 RUN cargo build --release
 
-# build for client program
+# build for client side
 RUN USER=root cargo new --bin client
 WORKDIR /client
 COPY ./client/Cargo* ./
 COPY ./client/src ./src
 RUN cargo build --release
 
-FROM ryusei1068/rust_ubuntu:latest
+FROM ubuntu:22.04 
 COPY --from=build /server/target/release/server ./
 COPY --from=build /client/target/release/client ./
 
 COPY ./local-chat.sh ./
 RUN chmod 755 local-chat.sh
 
-# CMD ./local-chat.sh
+ENTRYPOINT ./local-chat.sh 
--- client/src/main.rs
+++ client/src/main.rs
@@ -2,6 +2,25 @@ use std::io::prelude::*;
 use std::os::unix::net::UnixStream;
 use std::time::Duration;
 
+fn write_request_and_shutdown(stream: &mut UnixStream) -> std::io::Result<()> {
+    stream.write(b"Hello server")?;
+
+    println!("We sent a request");
+    println!("Shutting down writing on the stream, waiting for response...");
+
+    // stream.shutdown(std::net::Shutdown::Write)?;
+
+    Ok(())
+}
+
+fn read_from_stream(stream: &mut UnixStream) -> std::io::Result<()> {
+    let mut response = String::new();
+    stream.read_to_string(&mut response)?;
+
+    println!("We received this response: {}", response);
+    Ok(())
+}
+
 fn main() -> std::io::Result<()> {
     println!("hello client");
 
@@ -15,10 +34,8 @@ fn main() -> std::io::Result<()> {
         .set_read_timeout(Some(Duration::new(2, 0)))
         .expect("Couldn't set read timeout");
 
-    stream.write_all(b"hello world")?;
+    write_request_and_shutdown(&mut stream)?;
+    read_from_stream(&mut stream)?;
 
-    let mut response = String::new();
-    stream.read_to_string(&mut response)?;
-    println!("{response}");
     Ok(())
 }
